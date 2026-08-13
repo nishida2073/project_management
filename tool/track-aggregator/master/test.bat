1a697a5336e332a683587c4e66e2212c05d84e20
nishida2073 <ishida@support-s-frontier.com>
2026-08-13 14:01:19 +0900

    md

--- tool/track-aggregator/master/test.bat
+++ tool/track-aggregator/master/test.bat
@@ -1,7 +1,4 @@
 @echo off
 
-set "KintoneID="
-set "KintonePW="
-set "Authorization=U3RkMjAyNl9TRlQ6U3RkMjAyNl9TRlQ="
 set "KintoneSubdomain=univ-kyousai-l"
 set "BaseUrl=https://%KintoneSubdomain%.cybozu.com"
