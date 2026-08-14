bd060f5a9d24d910551b9f7894f0cabc64bec255
nishida2073 <ishida@support-s-frontier.com>
2026-08-14 21:15:58 +0900

    md

--- tool/track-aggregator/master/地域共催.bat
+++ tool/track-aggregator/master/地域共催.bat
@@ -2,3 +2,10 @@
 
 set "KintoneSubdomain=univ-kyousai-l"
 set "BaseUrl=https://%KintoneSubdomain%.cybozu.com"
+
+set "CourseGroup1Name=ビジネス"
+set "CourseGroup1Courses=ビジネス基礎,ビジネス応用"
+set "CourseGroup2Name=IT技術基礎"
+set "CourseGroup2Courses=社会を支えるITサービス,データベース技術入門,Web技術入門"
+set "CourseGroup3Name=プログラミング"
+set "CourseGroup3Courses=アルゴリズム入門,プログラミング基礎,プログラミング応用"
