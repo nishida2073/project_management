d2ba8f35f89df8a69984dba35ea3d95d2546bfdf
nishida2073 <ishida@support-s-frontier.com>
2026-08-14 12:20:49 +0900

    md

---
+++ tool/track-aggregator/bats/download-results.bat
@@ -0,0 +1,52 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "MyName=%~nx0"
+
+call "%~dp0common-env.bat"
+
+set "SCRIPT_PATH=%~dp0download-results.ps1"
+
+set "DownloadDetail=1"
+
+for %%F in ("%MasterDataRootDir%\*.xlsx") do (
+    call "%~dp0message.bat" "Start %MyName% [%%~nF]"
+    
+    set "envFile=%MasterDataRootDir%\%%~nF.bat"
+    if exist "!envFile!" (
+        call "!envFile!"
+        
+        set "JOB_FLAG=%TEMP%\%MyName%%%~nF_.running"
+        
+        start "" /b powershell -NoProfile -ExecutionPolicy Bypass -Command ^
+          "New-Item -Path '!JOB_FLAG!' -ItemType File -Force | Out-Null;" ^
+          "& '%SCRIPT_PATH%'" ^
+          "   -MasterDataFilePath '%%F'" ^
+          "   -AutoHotkeyExePath '%AutoHotkeyExePath%'" ^
+          "   -AutoHotkeyScriptPath '%AutoHotkeyScriptPath%'" ^
+          "   -TargetGroupName '%%~nF'" ^
+          "   -TestResultRootDir '%TestResultRootDir%'" ^
+          "   -SurveyResultRootDir '%SurveyResultRootDir%'" ^
+          "   -DownloadDetail '%DownloadDetail%';" ^
+          "Remove-Item -Path '!JOB_FLAG!' -Force;"
+        
+    ) else (
+        call "%~dp0message.bat" "ŠÂ‹«Ý’èƒtƒ@ƒCƒ‹‚ªŒ©‚Â‚©‚è‚Ü‚¹‚ñ: !envFile!" "Red"
+    )
+    call "%~dp0message.bat" "Finished %MyName% [%%~nF]"
+)
+
+call "%~dp0message.bat" "Start Jobs %MyName% ALL"
+
+:WAIT_LOOP
+set "ALL_DONE=1"
+for %%F in ("%MasterDataRootDir%\*.xlsx") do (
+    set "JOB_FLAG=%TEMP%\%MyName%%%~nF_%TargetDate%.running"
+    if exist "!JOB_FLAG!" set "ALL_DONE=0"
+)
+if !ALL_DONE! EQU 0 (
+    timeout /t 1 >nul
+    goto WAIT_LOOP
+)
+
+call "%~dp0message.bat" "Finished Jobs %MyName% ALL"
---
+++ tool/track-aggregator/bats/download-results.ps1
@@ -0,0 +1,27 @@
+﻿param(
+    [string]$MasterDataFilePath,
+    [string]$AutoHotkeyExePath,
+    [string]$AutoHotkeyScriptPath,
+    [string]$TargetGroupName,
+    [string]$TestResultRootDir,
+    [string]$SurveyResultRootDir,
+    [int]$DownloadDetail
+)
+
+$libraryDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$libraryDir = Join-Path $libraryDir "library"
+Get-ChildItem -Path $libraryDir -Filter *.psm1 -Recurse | ForEach-Object {
+    Import-Module $_.FullName -ErrorAction Stop -DisableNameChecking
+}
+
+$downloadDetail = if($DownloadDetail -eq 1){ $true } else { $false }
+
+$testDatas = Create-TestDatas -DataFilePath $MasterDataFilePath
+$testDatas = @($testDatas | Where-Object { -not (ToBool $_.停止中) })
+
+$surveyDatas = Create-SurveyDatas -DataFilePath $MasterDataFilePath
+$surveyDatas = @($surveyDatas | Where-Object { -not (ToBool $_.停止中) })
+
+Download-TrackResults -AutoHotkeyExePath $AutoHotkeyExePath -AutoHotkeyScriptPath $AutoHotkeyScriptPath -TargetRootDir $TestResultRootDir -TargetGroupName $TargetGroupName -Datas $testDatas -NameProperty "testName" -IsDetail $downloadDetail
+
+Download-TrackResults -AutoHotkeyExePath $AutoHotkeyExePath -AutoHotkeyScriptPath $AutoHotkeyScriptPath -TargetRootDir $SurveyResultRootDir -TargetGroupName $TargetGroupName -Datas $surveyDatas -NameProperty "surveyName"
