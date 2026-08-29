8ef58ad713e358866e788c874db3908e79ba2a72
nishida2073 <ishida@support-s-frontier.com>
2026-08-29 14:12:35 +0900

    md

--- tool/track-aggregator/bats/message.bat
+++ tool/track-aggregator/bats/message.bat
@@ -8,5 +8,5 @@ if "%COLOR%"=="" (
     set "COLOR=White"
 )
 
-powershell -NoProfile -Command ^
-    "$color = '%COLOR%'; $msg = '%MESSAGE%'; Write-Host ('============== [' + (Get-Date).ToString('yyyy-MM-dd HH:mm:ss.ff') + '] ' + $msg + ' ==============') -ForegroundColor $color"
+powershell -NoProfile -ExecutionPolicy Bypass -Command ^
+    "Import-Module '%~dp0library\common.psm1' -DisableNameChecking; $color = '%COLOR%'; $msg = '%MESSAGE%'; Write-Message ('============== [' + (Get-Date).ToString('yyyy-MM-dd HH:mm:ss.ff') + '] ' + $msg + ' ==============') -Type Info -ForegroundColor $color -NoHeader"
