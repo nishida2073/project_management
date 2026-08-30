152878d792607f81e1587440ff53e322e6887a0a
nishida2073 <ishida@support-s-frontier.com>
2026-08-30 20:57:25 +0900

    md

--- tmp/生成イメージ/html/js/select.js
+++ tmp/生成イメージ/html/js/select.js
@@ -48,6 +48,40 @@
       if (target && map[value] !== undefined) target.textContent = map[value];
     }
 
+    var target2Selector = el.getAttribute('data-target2');
+    var map2Attr = el.getAttribute('data-map2');
+    if (target2Selector && map2Attr) {
+      var target2 = document.querySelector(target2Selector);
+      var map2 = JSON.parse(map2Attr);
+      if (target2 && map2[value] !== undefined) {
+        var mapped2 = map2[value];
+        target2.textContent = mapped2;
+        var classMap2Attr = el.getAttribute('data-class-map2');
+        if (classMap2Attr) {
+          var classMap2 = JSON.parse(classMap2Attr);
+          Object.keys(classMap2).forEach(function (k) { target2.classList.remove(classMap2[k]); });
+          if (classMap2[mapped2]) target2.classList.add(classMap2[mapped2]);
+        }
+      }
+    }
+
+    var target3Selector = el.getAttribute('data-target3');
+    var map3Attr = el.getAttribute('data-map3');
+    if (target3Selector && map3Attr) {
+      var target3 = document.querySelector(target3Selector);
+      var map3 = JSON.parse(map3Attr);
+      if (target3 && map3[value] !== undefined) {
+        var mapped3 = map3[value];
+        target3.textContent = mapped3;
+        var classMap3Attr = el.getAttribute('data-class-map3');
+        if (classMap3Attr) {
+          var classMap3 = JSON.parse(classMap3Attr);
+          Object.keys(classMap3).forEach(function (k) { target3.classList.remove(classMap3[k]); });
+          if (classMap3[mapped3]) target3.classList.add(classMap3[mapped3]);
+        }
+      }
+    }
+
     var classMapAttr = el.getAttribute('data-class-map');
     if (classMapAttr) {
       var classMap = JSON.parse(classMapAttr);
@@ -57,15 +91,67 @@
 
     var optionsMapAttr = el.getAttribute('data-options-map');
     if (targetSelector && optionsMapAttr) {
-      var chipBox = document.querySelector(targetSelector);
+      var mapTarget = document.querySelector(targetSelector);
       var optionsMap = JSON.parse(optionsMapAttr);
-      if (chipBox && optionsMap[value] && window.chipSelectSetOptions) {
-        window.chipSelectSetOptions(chipBox, optionsMap[value]);
+      if (mapTarget && optionsMap[value] !== undefined) {
+        if (mapTarget.classList.contains('js-chip-select') && window.chipSelectSetOptions) {
+          window.chipSelectSetOptions(mapTarget, optionsMap[value]);
+        } else if (mapTarget.classList.contains('js-select')) {
+          jsSelectSetOptions(mapTarget, optionsMap[value]);
+        }
       }
     }
   }
 
-  document.querySelectorAll('.js-select').forEach(function (el) {
+  function jsSelectSetOptions(el, newOptions) {
+    el.setAttribute('data-options', newOptions.join(','));
+    var current = el.getAttribute('data-value') || '';
+    if (current && newOptions.indexOf(current) === -1) {
+      el.removeAttribute('data-value');
+      var caret = el.querySelector('.caret');
+      el.textContent = '選択してください';
+      if (caret) el.appendChild(caret);
+      else el.insertAdjacentHTML('beforeend', '<span class="caret">▾</span>');
+
+      var targetSelector = el.getAttribute('data-target');
+      var mapAttr = el.getAttribute('data-map');
+      if (targetSelector && mapAttr) {
+        var target = document.querySelector(targetSelector);
+        if (target) target.textContent = '選択すると自動入力されます';
+      }
+
+      var target2Selector = el.getAttribute('data-target2');
+      var map2Attr = el.getAttribute('data-map2');
+      if (target2Selector && map2Attr) {
+        var target2 = document.querySelector(target2Selector);
+        if (target2) {
+          target2.textContent = '';
+          var classMap2Attr = el.getAttribute('data-class-map2');
+          if (classMap2Attr) {
+            var classMap2 = JSON.parse(classMap2Attr);
+            Object.keys(classMap2).forEach(function (k) { target2.classList.remove(classMap2[k]); });
+          }
+        }
+      }
+
+      var target3Selector = el.getAttribute('data-target3');
+      var map3Attr = el.getAttribute('data-map3');
+      if (target3Selector && map3Attr) {
+        var target3 = document.querySelector(target3Selector);
+        if (target3) {
+          target3.textContent = '';
+          var classMap3Attr = el.getAttribute('data-class-map3');
+          if (classMap3Attr) {
+            var classMap3 = JSON.parse(classMap3Attr);
+            Object.keys(classMap3).forEach(function (k) { target3.classList.remove(classMap3[k]); });
+          }
+        }
+      }
+    }
+  }
+  window.jsSelectSetOptions = jsSelectSetOptions;
+
+  function bindSelect(el) {
     if (!el.hasAttribute('data-value')) {
       var firstNode = el.childNodes[0];
       var text = (firstNode && firstNode.nodeType === 3 ? firstNode.textContent : '').trim();
@@ -79,9 +165,28 @@
       el.appendChild(buildMenu(el));
       el.classList.add('open');
     });
-  });
+  }
+  window.bindJsSelect = bindSelect;
+
+  document.querySelectorAll('.js-select').forEach(bindSelect);
 
   document.addEventListener('click', function () {
     closeOthers();
+  });
+
+  document.querySelectorAll('.radio-group[data-target][data-options-map]').forEach(function (group) {
+    var targetSelector = group.getAttribute('data-target');
+    var optionsMap = JSON.parse(group.getAttribute('data-options-map'));
+    group.querySelectorAll('input[type="radio"]').forEach(function (radio) {
+      radio.addEventListener('change', function () {
+        var target = document.querySelector(targetSelector);
+        if (!target || optionsMap[radio.value] === undefined) return;
+        if (target.classList.contains('js-chip-select') && window.chipSelectSetOptions) {
+          window.chipSelectSetOptions(target, optionsMap[radio.value]);
+        } else if (target.classList.contains('js-select')) {
+          jsSelectSetOptions(target, optionsMap[radio.value]);
+        }
+      });
+    });
   });
 })();
