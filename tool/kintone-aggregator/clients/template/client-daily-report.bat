3098d1648fc6aa569385ec7dd53b88a62a4d67f9
nishida2073 <ishida@support-s-frontier.com>
2026-08-31 23:39:16 +0900

    md

---
+++ tool/kintone-aggregator/clients/template/client-daily-report.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set "TargetAppIds="
+set "TargetDateCodeField=日付"
+set "TargetUserCodeField=個人ID"
+set "TargetFixedCodeFields=通番,会社名,受講生ID,氏名,科目名,日付"
+set "TargetAppCodeFields=本日の理解度"
+set "TargetSummaryCodeFields=クラス名"
+
---
+++ tool/kintone-aggregator/clients/template/client.bat
@@ -0,0 +1,7 @@
+@echo off
+
+set "KintoneID="
+set "KintonePW="
+set "Authorization="
+set "KintoneSubdomain=univ-kyousai-{x}"
+set "BaseUrl=https://%KintoneSubdomain%.cybozu.com"
