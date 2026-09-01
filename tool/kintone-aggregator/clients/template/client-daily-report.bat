7b1f570c3c53366277872a391bef300d1d939919
nishida2073 <ishida@support-s-frontier.com>
2026-09-01 10:11:53 +0900

    md

--- tool/kintone-aggregator/clients/template/client-daily-report.bat
+++ tool/kintone-aggregator/clients/template/client-daily-report.bat
@@ -3,6 +3,6 @@
 set "TargetAppIds="
 set "TargetDateCodeField=日付"
 set "TargetUserCodeField=個人ID"
-set "TargetFixedCodeFields=通番,会社名,受講生ID,氏名,科目名,日付"
-set "TargetAppCodeFields=本日の理解度"
+set "TargetFixedCodeFields=*"
+set "TargetAppCodeFields=*"
 
