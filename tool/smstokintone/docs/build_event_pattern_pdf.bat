db97844a3f50ef5fe85ca534b2e1d34adb0ad7b0
nishida2073 <ishida@support-s-frontier.com>
2026-09-06 11:58:15 +0900

    md

--- tool/smstokintone/docs/build_event_pattern_pdf.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-node "..\..\md2png\build_pdf.js" "開催パターン別設定.md" "開催パターン別設定.pdf"
-endlocal
--- tool/smstokintone/docs/build_manual_pdf.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-node "..\..\md2png\build_pdf.js" "マニュアル.md" "マニュアル.pdf" --title-page --narrow-margins
-endlocal
