90cd7ddc4189122c74e3d10765168a96c75e6af1
nishida2073 <ishida@support-s-frontier.com>
2026-09-08 13:06:50 +0900

    md

--- tool/macro/build_pdf-実績反映機能_マニュアル.bat
+++ tool/macro/build_pdf-実績反映機能_マニュアル.bat
@@ -3,5 +3,5 @@ setlocal
 cd /d "%~dp0"
 set "NAME=%~n0"
 set "NAME=%NAME:build_pdf-=%"
-node "..\md2png\build_pdf.js" "%NAME%.md" "%NAME%.pdf" --title-page --narrow-margins --page-numbers
+node "..\md2png\build_pdf.js" "%NAME%.md" "%NAME%.pdf" --title-page --narrow-margins --page-numbers --extra-css="%NAME%.css"
 endlocal
