3584f228d822db3d7e0ab0a51681e45bde356be0
nishida2073 <ishida@support-s-frontier.com>
2026-09-11 08:35:40 +0900

    ref

--- tool/macro/import-macros.bat
+++ tool/macro/import-macros.bat
@@ -1,5 +1,5 @@
 @echo off
 setlocal
 set "SCRIPT_DIR=%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT_DIR%import-macros.ps1" %*
+powershell -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT_DIR%import-macros.ps1" -MacroDir "%SCRIPT_DIR%macros" %*
 endlocal
