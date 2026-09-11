57c03de6dd28285c61b48306a021c0f7b8a2d7ff
nishida2073 <ishida@support-s-frontier.com>
2026-09-11 19:38:31 +0900

    md

---
+++ tool/macro/ルールの再作成/build_pdf-README_rebuild-conditional-formatting.bat
@@ -0,0 +1,7 @@
+@echo off
+setlocal
+cd /d "%~dp0"
+set "NAME=%~n0"
+set "NAME=%NAME:build_pdf-=%"
+node "..\..\md2png\build_pdf.js" "%NAME%.md" "%NAME%.pdf" --title-page --issue-date=2026/09 --issue-date-position=back --narrow-margins --toc -toc-depth=5 --bookmark-depth=5 --page-numbers
+endlocal
