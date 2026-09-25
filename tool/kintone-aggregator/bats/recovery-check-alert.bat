874f1c6af6abe3eb4c5cb7a64642d4c249162508
nishida2073 <ishida@support-s-frontier.com>
2026-09-26 03:17:57 +0900

    md

--- tool/kintone-aggregator/bats/recovery-check-alert.bat
+++ tool/kintone-aggregator/bats/recovery-check-alert.bat
@@ -6,8 +6,8 @@ call "%~dp0message.bat" "Start %MyName%"
 
 call "%~dp0common-env.bat"
 
-call "%~dp0..\create-app-data.bat" "%~1"
-call "%~dp0..\collect-app-data.bat" "%~1"
+call "%~dp0..\create-app-data.bat" "%~1" "" "%~2"
+call "%~dp0..\collect-app-data.bat" "%~1" "" "%~2"
 
 call "%~dp0message.bat" "Finished %MyName%"
 
